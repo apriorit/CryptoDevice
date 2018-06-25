c11bdae8ede353175ed5503fe7ac4b46331f4e08
Artem Kotovsky <kotovsky@apriorit.com>
2018-06-25 13:08:58 +0300

    changes according to protocol

--- src/Common/CryptoDeviceProtocol.h
+++ src/Common/CryptoDeviceProtocol.h
@@ -6,6 +6,8 @@
 
 #pragma pack(push, 1) // all data must be aligned by 1 byte
 
+#define CRYPTO_DEVICE_DMA_MAGIC_BYTE 0x11
+#define CRYPTO_DEVICE_PAGE_MASK 0xFFF
 #define CRYPTO_DEVICE_PAGE_SIZE 0x1000
 #define CRYPTO_DEVICE_PAGE_SHIFT 0x0C
 #define CRYPTO_DEVICE_TO_PHYS($dma) (uint64_t)(((uint64_t)($dma)) << CRYPTO_DEVICE_PAGE_SHIFT)
@@ -14,9 +16,14 @@
 
 typedef enum tagCryptoDeviceErrorCode
 {
-    CryptoDevice_NoError    = 0x00,
-    CryptoDevice_DmaError   = 0x01,
-    CryptoDevice_ResetError = 0x02
+    CryptoDevice_NoError       = 0x00,
+    CryptoDevice_DmaError      = 0x01,
+    CryptoDevice_ResetError    = 0x02,
+    CryptoDevice_WriteIoError  = 0x03,
+    CryptoDevice_InternalError = 0x04,
+
+    CryptoDevice_DeviceHasBeenReseted = -1
+
 } CryptoDeviceErrorCode;
 
 typedef enum tagCryptoDeviceState
@@ -31,17 +38,15 @@ typedef enum tagCryptoDeviceCommand
 {
     CryptoDevice_IdleCommand            = 0x00,
     CryptoDevice_ResetCommand           = 0x01,
-    CryptoDevice_AesEncryptCbcCommand   = 0x02,
-    CryptoDevice_AesDecryptCbcCommand   = 0x03,
+    CryptoDevice_AesCbcEncryptCommand   = 0x02,
+    CryptoDevice_AesCbcDecryptCommand   = 0x03,
     CryptoDevice_Sha2Command            = 0x04
-
 } CryptoDeviceCommand;
 
 typedef enum tagCryptoDeviceInterruptFlag
 {
     CryptoDevice_DisableFlag    = 0x00,
     CryptoDevice_EnableAllFlag  = 0xff
-
 } CryptoDeviceInterruptFlag;
 
 typedef enum tagCryptoDeviceMSI
@@ -51,7 +56,6 @@ typedef enum tagCryptoDeviceMSI
     CryptoDevice_MsiReady  = 0x02,
     CryptoDevice_MsiReset  = 0x03,
     CryptoDevice_MsiMax    = 0x04
-
 } CryptoDeviceMSI;
 
 typedef struct tagCryptoDeviceIo
@@ -60,14 +64,16 @@ typedef struct tagCryptoDeviceIo
     /*0x01*/ uint8_t  State;
     /*0x02*/ uint8_t  Command;
     /*0x03*/ uint8_t  InterruptFlag;
-    /*0x04*/ uint32_t DmaBufIn;
-    /*0x08*/ uint32_t DmaBufOut;
-    /*0x0C*/ uint32_t DmaCountIn;
-    /*0x10*/ uint32_t DmaCountOut;
-    /*0x14*/ uint8_t  MsiErrorFlag;
-    /*0x15*/ uint8_t  MsiReadyFlag;
-    /*0x16*/ uint8_t  MsiResetFlag;
-    /*0x17*/ uint8_t Unused;
+    /*0x04*/ uint32_t DmaInAddress;
+    /*0x08*/ uint32_t DmaInPagesCount;
+    /*0x0C*/ uint32_t DmaInSizeInBytes;
+    /*0x10*/ uint32_t DmaOutAddress;
+    /*0x14*/ uint32_t DmaOutPagesCount;
+    /*0x18*/ uint32_t DmaOutSizeInBytes;
+    /*0x1C*/ uint8_t  MsiErrorFlag;
+    /*0x1D*/ uint8_t  MsiReadyFlag;
+    /*0x1E*/ uint8_t  MsiResetFlag;
+    /*0x1F*/ uint8_t  Unused;
     
 } CryptoDeviceIo;
 
@@ -75,13 +81,15 @@ static_assert(0x00 == offsetof(CryptoDeviceIo, ErrorCode), "Invalid ErrorCode of
 static_assert(0x01 == offsetof(CryptoDeviceIo, State), "Invalid State offset");
 static_assert(0x02 == offsetof(CryptoDeviceIo, Command), "Invalid Command offset");
 static_assert(0x03 == offsetof(CryptoDeviceIo, InterruptFlag), "Invalid InterruptFlag offset");
-static_assert(0x04 == offsetof(CryptoDeviceIo, DmaBufIn), "Invalid DmaBufIn offset");
-static_assert(0x08 == offsetof(CryptoDeviceIo, DmaBufOut), "Invalid DmaBufOut offset");
-static_assert(0x0C == offsetof(CryptoDeviceIo, DmaCountIn), "Invalid DmaCountIn offset");
-static_assert(0x10 == offsetof(CryptoDeviceIo, DmaCountOut), "Invalid DmaCountOut offset");
-static_assert(0x14 == offsetof(CryptoDeviceIo, MsiErrorFlag), "Invalid MsiErrorFlag offset");
-static_assert(0x15 == offsetof(CryptoDeviceIo, MsiReadyFlag), "Invalid MsiReadyFlag offset");
-static_assert(0x16 == offsetof(CryptoDeviceIo, MsiResetFlag), "Invalid MsiResetFlag offset");
-static_assert(0x17 == offsetof(CryptoDeviceIo, Unused), "Invalid Unused offset");
+static_assert(0x04 == offsetof(CryptoDeviceIo, DmaInAddress), "Invalid DmaInAddress offset");
+static_assert(0x08 == offsetof(CryptoDeviceIo, DmaInPagesCount), "Invalid DmaInPagesCount offset");
+static_assert(0x0C == offsetof(CryptoDeviceIo, DmaInSizeInBytes), "Invalid DmaInSizeInBytes offset");
+static_assert(0x10 == offsetof(CryptoDeviceIo, DmaOutAddress), "Invalid DmaOutAddress offset");
+static_assert(0x14 == offsetof(CryptoDeviceIo, DmaOutPagesCount), "Invalid DmaOutPagesCount offset");
+static_assert(0x18 == offsetof(CryptoDeviceIo, DmaOutSizeInBytes), "Invalid DmaOutSizeInBytes offset");
+static_assert(0x1C == offsetof(CryptoDeviceIo, MsiErrorFlag), "Invalid MsiErrorFlag offset");
+static_assert(0x1D == offsetof(CryptoDeviceIo, MsiReadyFlag), "Invalid MsiReadyFlag offset");
+static_assert(0x1E == offsetof(CryptoDeviceIo, MsiResetFlag), "Invalid MsiResetFlag offset");
+static_assert(0x1F == offsetof(CryptoDeviceIo, Unused), "Invalid Unused offset");
 
 #pragma pack(pop)
